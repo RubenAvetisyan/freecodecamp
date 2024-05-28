a2d9c19b9132b3b06c942f786d50bc9b3033c43c
Ruben Avetisyan <arbods.ruben@gmail.com>
2024-05-28 13:45:36 +0000

    task

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
 
-app.use(cors())
-app.use(express.static('public'))
+app.use(cors());
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+let users = [];
+let exercises = [];
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(__dirname + '/views/index.html');
 });
 
+// Create a new user
+app.post('/api/users', (req, res) => {
+  const username = req.body.username;
+  const newUser = { username, _id: Date.now().toString() };
+  users.push(newUser);
+  res.json(newUser);
+});
 
+// Get a list of all users
+app.get('/api/users', (req, res) => {
+  res.json(users);
+});
 
-
-
+// Start the server
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log('Your app is listening on port ' + listener.address().port);
+});
