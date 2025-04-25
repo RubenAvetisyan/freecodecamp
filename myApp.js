106ed39f802b5a053f85ae067b29f292f89508d2
Ruben Avetisyan <arbods.ruben@gmail.com>
2025-04-25 05:42:37 +0000

    init

--- myApp.js
+++ myApp.js
@@ -1,4 +1,17 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
+
+(async function (){
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+  } catch (error) {
+    throw error
+  }
+})();
+
 
 
 let Person;
